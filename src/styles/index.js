68cef7ab0a99432ac0a01186b0c25afebe46163f
Nagge <artnagge@gmail.com>
2019-07-04 16:44:30 +0500

    Publish
    First deploy

---
+++ src/styles/index.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from "react-native";
+
+const stylesMain = StyleSheet.create({
+    welcome: {
+        fontSize: 20,
+        fontWeight: "600"
+    }
+});
+
+export {
+    stylesMain
+};
